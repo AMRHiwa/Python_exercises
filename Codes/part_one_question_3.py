fb3f15102db3b205920012ce06a4d3682e4a245b
Mohammad Rasoul Azizi <azizi.mr1377@gmail.com>
2024-03-12 19:50:31 +0330

    add comments for part_one_question_3

--- Codes/part_one_question_3.py
+++ Codes/part_one_question_3.py
@@ -1,4 +1,8 @@
+# importing library
 import math
+
+# getting wind speed and temp
 wind_speed, temp = map(int, input("Enter speed and tempture: ").split() )
 
+# calculate of wind chill and show it in output
 print(f"Wind Chill: {13.12 + 0.6215 * temp - 11.37 * math.pow(wind_speed, 0.16) + 0.3965 * temp * math.pow(wind_speed, 0.16)}")
